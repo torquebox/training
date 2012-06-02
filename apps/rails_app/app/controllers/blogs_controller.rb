a5873567978bb4c38ed1140a066300c4c02566d5
Bob McWhirter <bob@mcwhirter.org>
2012-06-02 14:27:48 -0400

    Fixing some more nested-resource paths.

--- apps/rails_app/app/controllers/blogs_controller.rb
+++ apps/rails_app/app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.where( :user_id=>@user )
 
     respond_to do |format|
       format.html # index.html.erb
