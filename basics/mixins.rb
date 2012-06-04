09a58979701b9a896be6930cf5b267f54e40d7aa
Bob McWhirter <bob@mcwhirter.org>
2012-06-04 09:34:49 -0400

    User-suggested changes.

--- basics/mixins.rb
+++ basics/mixins.rb
@@ -5,12 +5,23 @@ module MagicalPowers
   end
 end
 
-class MagicalHuman
+module RightHanded
+  def loosen_a_screw
+    puts "lefty loosey"
+  end
+end
+
+class Animal
+end
+
+class MagicalHuman < Animal
   include MagicalPowers
+  include RightHanded
 end
 
 magical_human = MagicalHuman.new
 magical_human.fly
+magical_human.loosen_a_screw
 
 class MundaneHuman
 end
